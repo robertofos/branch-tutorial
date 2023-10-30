43aeec102f9a059d08aee784d1cdea7b7acc6251
robertofos <robertofos@gmail.com>
2023-10-30 17:36:09 -0300

    teste

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const modificacao = 'Meu primeiro commit'
 cons joao = "oi eu seu o joao"
 const main = 'Meu primeiro commit'
-const carlinho = 'Eu sou o carlinho'
+const carlinho = 'Eu sou o Carlinho'
